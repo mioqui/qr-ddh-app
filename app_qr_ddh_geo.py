96ad9cf1cfe7a355f035dcf186aadadf7312bb9c
mioqui <65793519+mioqui@users.noreply.github.com>
2025-07-24 08:16:15 -0500

    Update app_qr_ddh_geo.py
    se agrego la linea de la carpeta fonts

--- app_qr_ddh_geo.py
+++ app_qr_ddh_geo.py
@@ -28,6 +28,7 @@ if uploaded_excel:
     if seleccion:
         fila = df[df["Cod Sondaje"] == seleccion].iloc[0]
         texto = f"{fila['Cod Sondaje']} | {fila['Veta']} | {fila['Nivel']}"
+        font_path = os.path.join("fonts", "arial.ttf")
         fuente = ImageFont.truetype("Arial.ttf", 42) if os.path.exists("Arial.ttf") else ImageFont.load_default()
 
         # Crear QR (convertimos a string todos los valores)
